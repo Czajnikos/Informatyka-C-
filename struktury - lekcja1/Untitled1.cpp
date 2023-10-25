095b279563b3c7b308866fe8fd167a4ecb8686b3
Czajnikos <dunduncsgo@gmail.com>
2023-10-25 14:10:32 +0200

    uwu

---
+++ struktury - lekcja1/Untitled1.cpp
@@ -0,0 +1,25 @@
+  #include <iostream> 
+  using namespace std;
+  int main(){
+  	
+  	short liczba = 2;
+  	float zmiennaprzecinkowa = 3.532;
+  	string tekst = "tekst";  
+  	char znak = 'A';
+  	bool znakowy = false;
+  	
+  	
+  	cout <<"Hello Jelly"<< endl;
+	cout <<"Liczba calkowita "<< liczba;
+	cout <<endl;
+	
+	
+	string jakos;
+	cout <<"Podaj jakis wyraz: ";
+  	cin >> jakos;
+  	cout <<"Wprowadziles wartosc  "  << jakos;
+  	
+  	
+  	
+  	return 0;
+  }
