fc4a3c1876e9d7c5538dad23a3fe076393ffdfd1
Czajnikos <dunduncsgo@gmail.com>
2023-11-09 15:04:30 +0100

    uwu

---
+++ petle - lekcja 3/petle - lekcja 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	/*	inkramentacja
+		i = i + 1
+		i += 1
+		i++
+		dekramentacja
+		i = i -1
+		i -= 1
+		i--
+		for (wartoscPoczotkowa; wartoscKonczaca; krok)
+	*/
+	
+   // for(int i = 0; i< 20; i+= 2){
+	//}
+//}
+	//petla zagniezdzona
+	for(int i = 0; i<6; i++){
+		for (int j = 0; j <3; j++){
+			cout << "*";
+		}
+	cout << endl;
+	}
+}
