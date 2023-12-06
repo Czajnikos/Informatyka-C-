aa51e812f4e8c6a34ac3f531b8966fac4efc831f
Czajnikos <dunduncsgo@gmail.com>
2023-12-06 14:08:34 +0100

    Komentarz

---
+++ tablice zmienne/tablice2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+int tab[10];
+cout << "podaj 5liczb: "; 
+for (int i=0;i<5;i++){
+        cin >> tab[i] ; 
+
+ 
+    }
+    cout << "na odwrot: ";
+	for(int i=0;i<5;i++){
+		if(tab[i] <= 0){
+			cout << "0 "; 
+			}
+			else cout << tab[i] << " ";
+	}
+}
